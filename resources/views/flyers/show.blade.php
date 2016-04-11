ca61fefb73ae863e4ce12392fe3fc1ab306349c5
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-04-11 16:14:45 +0300

    Added lightbox to flyer images

--- resources/views/flyers/show.blade.php
+++ resources/views/flyers/show.blade.php
@@ -22,7 +22,9 @@
                     @foreach($set as $photo)
 
                         <div class="col-md-3 gallery__image">
-                            <img src="/{{ $photo->thumbnail_path }}" alt="" class="img-responsive" />
+                            <a href="/{{ $photo->path }}" data-lity>
+                                <img src="/{{ $photo->thumbnail_path }}" alt="" class="img-responsive" />
+                            </a>
                         </div>
 
                     @endforeach
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -44,6 +44,8 @@
         @yield('content')
     </div>
 
+    <script   src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+
     <script src="/js/libs.js"></script>
 
     @yield('scripts.footer')
